5f71071306a4ff5df9188b51f30fa176a108b0ec
mhon2014 <mhon2014@my.fit.edu>
2023-06-05 00:31:27 -0400

    Spring Boot REST endpoints practice

--- springbootREST/src/main/java/com/example/springbootREST/controller/HelloController.java
+++ springbootREST/src/main/java/com/example/springbootREST/controller/HelloController.java
@@ -1,15 +1,43 @@
 package com.example.springbootREST.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.springbootREST.dto.Person;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class HelloController {
 
-    @RequestMapping(method = RequestMethod.GET, path = "/hello")
-    public String hello(){
-        return "Hello!";
+    @RequestMapping(method = RequestMethod.GET, path = "/hello/{name}")
+    public String hello(@PathVariable("name") String name){
+        return "Hello, " + name + "!" ;
     }
+
+    @GetMapping(path = "/hello")
+    public String hello(){
+        return "Hello";
+    }
+
+    @PostMapping(path = "/goodbye")
+    public String goodbye(@RequestBody Person p){
+        return "Goodbye, " + p.getName() + "!";
+    }
+
+    @GetMapping(path = "/getall")
+    public List<Person> getAllPerson(){
+        Person p1 = new Person("Bill");
+        Person p2 = new Person("Bob");
+        return Arrays.asList(p1,p2);
+    }
+
+    @GetMapping(path = "/statustest")
+    public void statusTest(HttpServletResponse response){
+        response.setStatus(HttpServletResponse.SC_NO_CONTENT);
+    }
+
+
 
 }
